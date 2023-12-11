86726c7e306b22d2a9751ce0e2926addc13be8fc
Junianto Wicaksono <juniantowicaksono06@gmail.com>
2023-12-11 10:00:37 +0700

    Finish setup

--- run.sh
+++ run.sh
@@ -3,9 +3,12 @@
 target_dir="/var/www/html"
 
 if [ "$(ls -A $folder_path)" ]; then
-    echo "Serving Folder is not empty running Apache2"
-    /usr/local/bin/apache2-foreground
+    echo "Serving Folder is not empty running PHP Artisan"
+    # /usr/local/bin/apache2-foreground
+    cd /var/www/html && php artisan serve --host=0.0.0.0
 else
     echo "Serving Folder is empty creating laravel from scratch"
-    cd /tmp && composer create-project laravel/laravel laravel1 && mv /tmp/laravel1/* /var/www/html
-    /usr/local/bin/apache2-foreground
+    cd /var/www/ && composer create-project laravel/laravel html
+    # /usr/local/bin/apache2-foreground
+    cd /var/www/html && php artisan serve --host=0.0.0.0
+fi
