f647d84991f83ecafa12782c8236ba162afef85c
Junianto Wicaksono <juniantowicaksono06@gmail.com>
2023-12-11 10:44:54 +0700

    Finishing config

--- run.sh
+++ run.sh
@@ -5,10 +5,11 @@ target_dir="/var/www/html"
 if [ "$(ls -A $folder_path)" ]; then
     echo "Serving Folder is not empty running PHP Artisan"
     # /usr/local/bin/apache2-foreground
-    cd /var/www/html && php artisan serve --host=0.0.0.0
 else
     echo "Serving Folder is empty creating laravel from scratch"
     cd /var/www/ && composer create-project laravel/laravel html
     # /usr/local/bin/apache2-foreground
-    cd /var/www/html && php artisan serve --host=0.0.0.0
 fi
+
+cd /var/www/html && php artisan serve --host=0.0.0.0
+cd /var/www/html && composer require inertiajs/inertia-laravel
